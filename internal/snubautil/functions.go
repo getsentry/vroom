80aba9412fe545db0c49a8906d35bfecc8fca5d6
Tony Xiao <txiao@sentry.io>
2022-07-21 12:32:39 -0400

    update select columns and response type

--- internal/snubautil/functions.go
+++ internal/snubautil/functions.go
@@ -8,14 +8,16 @@ import (
 
 type (
 	Function struct {
-		Name        string  `json:"name"`
-		Package     string  `json:"package"`
-		Path        string  `json:"path"`
-		Fingerprint uint64  `json:"fingerprint"`
-		P75         float64 `json:"p75"`
-		P95         float64 `json:"p95"`
-		P99         float64 `json:"p99"`
-		Count       uint64  `json:"count"`
+		Name        string   `json:"name"`
+		Package     string   `json:"package"`
+		Path        string   `json:"path"`
+		Fingerprint uint64   `json:"fingerprint"`
+		P75         float64  `json:"p75"`
+		P95         float64  `json:"p95"`
+		P99         float64  `json:"p99"`
+		Count       uint64   `json:"count"`
+		Worst       string   `json:"worst"`
+		Examples    []string `json:"examples"`
 	}
 
 	SnubaFunctionsResponse struct {
@@ -34,10 +36,11 @@ func GetFunctions(sqb QueryBuilder) ([]Function, error) {
 		"path",
 		"fingerprint",
 		"arrayElement(quantilesMerge(0.75)(percentiles), 1) AS p75",
-		"arrayElement(quantilesMerge(0.75)(percentiles), 1) AS p99",
+		"arrayElement(quantilesMerge(0.95)(percentiles), 1) AS p95",
+		"arrayElement(quantilesMerge(0.99)(percentiles), 1) AS p99",
 		"countMerge(count) AS count",
 		"argMaxMerge(worst) AS worst",
-		"groupUniqArray(5)(examples) AS examples",
+		"groupUniqArrayMerge(5)(examples) AS examples",
 	}
 	sqb.GroupBy = "name, package, path, fingerprint"
 
