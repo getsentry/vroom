374ac04700c94d1c4665a6d4f47dc1ee0e562c39
Pierre Massat <pierre.massat@sentry.io>
2022-07-26 18:49:13 -0400

    Start the GCS emulator when starting the dev server

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,5 +7,7 @@ export SENTRY_DSN="https://91f2762536314cbd9cc4a163fe072682@o1.ingest.sentry.io/
 export SENTRY_ENVIRONMENT="development"
 export SENTRY_PROFILES_BUCKET_NAME="sentry-profiles"
 export SENTRY_SNUBA_HOST="http://localhost:1218"
+export STORAGE_EMULATOR_HOST=http://0.0.0.0:8888/
 
+docker-compose -f ./deployments/docker-compose.yml up -d
 ./vroom
