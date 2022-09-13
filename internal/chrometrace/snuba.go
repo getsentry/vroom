36073c203e098c53283e138599761c9eceb7f59f
Pierre Massat <pierre.massat@sentry.io>
2022-09-13 13:45:06 -0700

    fix: Replace idle samples by common frames between two active samples

--- internal/chrometrace/snuba.go
+++ internal/chrometrace/snuba.go
@@ -86,6 +86,7 @@ func SpeedscopeFromSnuba(profile snubautil.Profile) ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
+		iosProfile.ReplaceIdleStacks()
 		p, err = iosSpeedscopeTraceFromProfile(&iosProfile)
 		if err != nil {
 			return nil, err
