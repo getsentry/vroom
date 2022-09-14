8dbefff5335a9bc383e0d1af27af1b8641206135
Tony Xiao <txiao@sentry.io>
2022-09-14 14:27:12 -0400

    fix(profiling): Dont append version code if empty
    If the version code is empty, we should not append a pair of empty parens.

--- internal/snubautil/version.go
+++ internal/snubautil/version.go
@@ -3,5 +3,8 @@ package snubautil
 import "fmt"
 
 func FormatVersion(name, code interface{}) string {
+	if code == "" {
+		return fmt.Sprintf("%v", name)
+	}
 	return fmt.Sprintf("%v (%v)", name, code)
 }
