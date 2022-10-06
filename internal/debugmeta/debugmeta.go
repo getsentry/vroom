5fb2bee770cf8c8411647bac5e7693b38fab404d
Pierre Massat <pierre.massat@sentry.io>
2022-10-06 15:55:47 -0600

    feat: Return missing debug images to display

---
+++ internal/debugmeta/debugmeta.go
@@ -0,0 +1,36 @@
+package debugmeta
+
+type (
+	Features struct {
+		HasDebugInfo  bool `json:"has_debug_info"`
+		HasSources    bool `json:"has_sources"`
+		HasSymbols    bool `json:"has_symbols"`
+		HasUnwindInfo bool `json:"has_unwind_info"`
+	}
+
+	Image struct {
+		Arch        string   `json:"arch"`
+		CodeFile    string   `json:"code_file"`
+		DebugID     string   `json:"debug_id"`
+		DebugStatus string   `json:"debug_status"`
+		Features    Features `json:"features"`
+		ImageAddr   string   `json:"image_addr"`
+		ImageSize   uint64   `json:"image_size"`
+		ImageVMAddr string   `json:"image_vmaddr"`
+		Type        string   `json:"type"`
+	}
+
+	DebugMeta struct {
+		Images []Image `json:"images"`
+	}
+)
+
+func (dm DebugMeta) MissingImages() []Image {
+	var missing []Image
+	for _, i := range dm.Images {
+		if i.DebugStatus != "found" {
+			missing = append(missing, i)
+		}
+	}
+	return missing
+}
