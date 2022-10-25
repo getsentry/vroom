e4c25afd19e99930c58054c6ac2ed52f43cdbaff
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-10-25 10:36:23 +0200

    Changes elapsed time to uint64

--- internal/measurements/measurements.go
+++ internal/measurements/measurements.go
@@ -1,7 +1,7 @@
 package measurements
 
 type Measurement struct {
-	ElapsedSinceStartNs int64   `json:"elapsed_since_start_ns"`
+	ElapsedSinceStartNs uint64  `json:"elapsed_since_start_ns"`
 	Unit                string  `json:"unit"`
 	Value               float64 `json:"value"`
 }
