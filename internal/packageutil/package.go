6227cf2b330668bde39c04b7a77a6e1794faaa60
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-11-04 14:31:07 +0100

    Fix IsApplicationPackage by passing the path instead of the BasePackageName

--- internal/packageutil/package.go
+++ internal/packageutil/package.go
@@ -4,27 +4,31 @@ import "strings"
 
 // isApplicationSymbol determines whether the image represents that of the application
 // binary (or a binary embedded in the application binary) by checking its path.
-func IsRustApplicationPackage(image string) bool {
+func IsRustApplicationPackage(path string) bool {
 	// `/library/std/src/` and `/usr/lib/system/` come from a real profile collected on macos.
-	// In this case the function belongs to a shared library,
-	// not to the profiled application
-	return !strings.Contains(image, "/library/std/src/") &&
-		!strings.HasPrefix(image, "/usr/lib/system/") &&
-		!(image == "")
+	// In this case the function belongs to a shared library, not to the profiled application.
+	return !strings.Contains(path, "/library/std/src/") &&
+		!strings.HasPrefix(path, "/usr/lib/system/") &&
+		// the following a prefixes of functions belonging to either core lib
+		// or third party libs
+		!strings.HasPrefix(path, "/rustc/") &&
+		!strings.HasPrefix(path, "/usr/local/rustup/") &&
+		!strings.HasPrefix(path, "/usr/local/cargo/git/") &&
+		!(path == "")
 }
 
 // isApplicationSymbol determines whether the image represents that of the application
 // binary (or a binary embedded in the application binary) by checking its path.
-func IsIOSApplicationPackage(image string) bool {
+func IsIOSApplicationPackage(path string) bool {
 	// These are the path patterns that iOS uses for applications, system
 	// libraries are stored elsewhere.
 	//
 	// Must be kept in sync with the corresponding Python implementation of
 	// this function in python/symbolicate/__init__.py
-	return strings.HasPrefix(image, "/private/var/containers") ||
-		strings.HasPrefix(image, "/var/containers") ||
-		strings.Contains(image, "/Developer/Xcode/DerivedData") ||
-		strings.Contains(image, "/data/Containers/Bundle/Application")
+	return strings.HasPrefix(path, "/private/var/containers") ||
+		strings.HasPrefix(path, "/var/containers") ||
+		strings.Contains(path, "/Developer/Xcode/DerivedData") ||
+		strings.Contains(path, "/data/Containers/Bundle/Application")
 }
 
 var (
