6b6099fb6d516fb8a11bb486a3ba2f1308580701
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-11-04 16:07:06 +0100

    Add comments to explain how we generate an address out of a frame

--- internal/sample/sample.go
+++ internal/sample/sample.go
@@ -124,6 +124,23 @@ func (f Frame) IsMain() (bool, int) {
 }
 
 func (f Frame) ID() string {
+	// When we have a symbolicated frame we can't rely on symbol_address
+	// to uniquely identify a frame since the following might happen:
+	//
+	// frame 1 has: sym_addr: 1, file: a.rs, line 2
+	// frame 2 has: sym_addr: 1, file: a.rs, line: 4
+	// because they have the same sym addr the second frame is reusing the first one,
+	// and gets the wrong line number
+	//
+	// Also, when a frame is symbolicated but is missing the symbol_address
+	// we know we're dealing with inlines, but we can't relt on instruction_address
+	// neither as the inlines are all using the same one. If we were to return this
+	// address in speedscope we would only generate a new frame for the parent one
+	// and for the inlines we would show the same information of the parents instead
+	// of their own
+	//
+	// As a solution here we use the following hash function that guarantees uniqueness
+	// when all the information required is available
 	hash := md5.Sum([]byte(fmt.Sprintf("%s:%s:%d:%s", f.File, f.Function, f.Line, f.InstructionAddr)))
 	return hex.EncodeToString(hash[:])
 }
