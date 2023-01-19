d867adb9794648b0e1e827812b610ef22d1f67fe
Pierre Massat <pierre.massat@sentry.io>
2023-01-19 11:18:00 -0500

    Add proper environment variables for occurrences

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -22,5 +22,7 @@ gcloud beta run deploy vroom \
   --set-env-vars=SENTRY_RELEASE="$git_commit_id" \
   --set-env-vars=SENTRY_SNUBA_HOST=http://snuba-api.profiling \
   --set-env-vars=SENTRY_OCCURRENCES_ENABLED_ORGANIZATIONS="1:,447951:" \
+  --set-env-vars=SENTRY_OCCURRENCES_KAFKA_BROKERS="specto-dev-kafka.service.us-central1.consul:9092" \
+  --set-env-vars=SENTRY_OCCURRENCES_KAFKA_TOPIC=ingest-occurrences \
   --timeout 30s \
   --vpc-connector sentry-ingest
