7d5fad7cfb26045eefb03d85ea6f88c320a9d131
Pierre Massat <pierre.massat@sentry.io>
2023-01-23 19:53:04 -0500

    feat: Add option to detect a frame above a threshold

--- internal/occurrence/detect_frame_test.go
+++ internal/occurrence/detect_frame_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/getsentry/vroom/internal/frame"
 	"github.com/getsentry/vroom/internal/nodetree"
-	"github.com/getsentry/vroom/internal/platform"
 	"github.com/getsentry/vroom/internal/testutil"
 )
 
@@ -14,11 +13,20 @@ func TestDetectFrameInCallTree(t *testing.T) {
 	trueValue := true
 	falseValue := false
 	tests := []struct {
+		job  DetectExactFrameOptions
 		name string
 		node *nodetree.Node
 		want map[nodeKey]nodeInfo
 	}{
 		{
+			job: DetectExactFrameOptions{
+				DurationThreshold: 16 * time.Millisecond,
+				FunctionsByPackage: map[string]map[string]struct{}{
+					"CoreFoundation": map[string]struct{}{
+						"CFReadStreamRead": {},
+					},
+				},
+			},
 			name: "Detect frame in call tree",
 			node: &nodetree.Node{
 				DurationNS:    uint64(30 * time.Millisecond),
@@ -159,13 +167,79 @@ func TestDetectFrameInCallTree(t *testing.T) {
 				},
 			},
 		},
+		{
+			job: DetectExactFrameOptions{
+				DurationThreshold: 16 * time.Millisecond,
+				FunctionsByPackage: map[string]map[string]struct{}{
+					"CoreFoundation": map[string]struct{}{
+						"CFReadStreamRead": {},
+					},
+					"vroom": map[string]struct{}{
+						"SuperShortFunction": {},
+					},
+				},
+			},
+			name: "Do not detect frame in call tree under threshold",
+			node: &nodetree.Node{
+				DurationNS:    uint64(30 * time.Millisecond),
+				EndNS:         uint64(30 * time.Millisecond),
+				Fingerprint:   0,
+				IsApplication: true,
+				Line:          0,
+				Name:          "root",
+				Package:       "package",
+				Path:          "path",
+				StartNS:       0,
+				Children: []*nodetree.Node{
+					&nodetree.Node{
+						DurationNS:    uint64(20 * time.Millisecond),
+						EndNS:         uint64(20 * time.Millisecond),
+						Fingerprint:   0,
+						IsApplication: false,
+						Line:          0,
+						Name:          "child1-1",
+						Package:       "package",
+						Path:          "path",
+						StartNS:       0,
+						Children: []*nodetree.Node{
+							&nodetree.Node{
+								DurationNS:    uint64(20 * time.Millisecond),
+								EndNS:         uint64(20 * time.Millisecond),
+								Fingerprint:   0,
+								IsApplication: true,
+								Line:          0,
+								Name:          "child2-1",
+								Package:       "package",
+								Path:          "path",
+								StartNS:       0,
+								Children: []*nodetree.Node{
+									&nodetree.Node{
+										DurationNS:    uint64(10 * time.Millisecond),
+										EndNS:         uint64(10 * time.Millisecond),
+										Fingerprint:   0,
+										IsApplication: false,
+										Line:          0,
+										Name:          "SuperShortFunction",
+										Package:       "vroom",
+										Path:          "path",
+										StartNS:       0,
+										Children:      []*nodetree.Node{},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: map[nodeKey]nodeInfo{},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			nodes := make(map[nodeKey]nodeInfo)
 			var stackTrace []frame.Frame
-			detectFrameInCallTree(tt.node, detectFrameJobs[platform.Cocoa][0], nodes, &stackTrace)
+			detectFrameInCallTree(tt.node, tt.job, nodes, &stackTrace)
 			if diff := testutil.Diff(nodes, tt.want); diff != "" {
 				t.Fatalf("Result mismatch: got - want +\n%s", diff)
 			}
