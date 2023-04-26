0f6995a5212dcc3b1676d005ae57aaf2a4430593
Pierre Massat <pierre.massat@sentry.io>
2023-04-26 18:17:54 -0400

    Pass an empty tag object instead

--- internal/occurrence/kafka.go
+++ internal/occurrence/kafka.go
@@ -9,11 +9,6 @@ import (
 func GenerateKafkaMessageBatch(occurrences []*Occurrence) ([]kafka.Message, error) {
 	messages := make([]kafka.Message, 0, len(occurrences))
 	for _, o := range occurrences {
-		// Skip event with no tag as they are considered invalid by the issue platform
-		if len(o.Event.Tags) == 0 {
-			continue
-		}
-
 		b, err := json.Marshal(o)
 		if err != nil {
 			return nil, err
