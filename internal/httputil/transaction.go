471c6c96bb37df91b4aea195707f135f173d4888
Pierre Massat <pierre.massat@sentry.io>
2023-06-12 10:45:13 -0400

    feat: Set status code tag on the transaction

---
+++ internal/httputil/transaction.go
@@ -0,0 +1,24 @@
+package httputil
+
+import (
+	"strconv"
+
+	"github.com/getsentry/sentry-go"
+)
+
+// HTTPStatusCodeTag is the name of the HTTP status code tag.
+const HTTPStatusCodeTag = "http.response.status_code"
+
+// SetHTTPStatusCodeTag sets the status code tag for the current request to the top-level transaction.
+func SetHTTPStatusCodeTag(e *sentry.Event, hint *sentry.EventHint) *sentry.Event {
+	if hint.Response == nil {
+		return e
+	}
+	if e.Tags == nil {
+		e.Tags = make(map[string]string)
+	}
+	if _, exists := e.Tags[HTTPStatusCodeTag]; !exists {
+		e.Tags[HTTPStatusCodeTag] = strconv.Itoa(hint.Response.StatusCode)
+	}
+	return e
+}
