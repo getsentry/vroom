182c41f6ca09f10ca4d1a65ac64a6135a208274d
Pierre Massat <pierre.massat@sentry.io>
2023-09-25 11:12:33 -0400

    fix: Close remaining open frames for Android call trees (#316)

---
+++ internal/profile/android_test.go
@@ -0,0 +1,430 @@
+package profile
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/getsentry/vroom/internal/frame"
+	"github.com/getsentry/vroom/internal/nodetree"
+	"github.com/getsentry/vroom/internal/speedscope"
+	"github.com/getsentry/vroom/internal/testutil"
+)
+
+var (
+	missingEnterEventsTrace = Android{
+		Clock: "Dual",
+		Events: []AndroidEvent{
+			{
+				Action:   "Enter",
+				ThreadID: 1,
+				MethodID: 1,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 1000,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Enter",
+				ThreadID: 1,
+				MethodID: 3,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 1500,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Enter",
+				ThreadID: 1,
+				MethodID: 4,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 1750,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Exit",
+				ThreadID: 1,
+				MethodID: 2,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 2000,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Exit",
+				ThreadID: 1,
+				MethodID: 4,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 2250,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Exit",
+				ThreadID: 1,
+				MethodID: 3,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 2500,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Exit",
+				ThreadID: 1,
+				MethodID: 1,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 3000,
+						},
+					},
+				},
+			},
+		},
+		Methods: []AndroidMethod{
+			{
+				ClassName: "class1",
+				ID:        1,
+				Name:      "method1",
+				Signature: "()",
+			},
+			{
+				ClassName: "class2",
+				ID:        2,
+				Name:      "method2",
+				Signature: "()",
+			},
+			{
+				ClassName: "class3",
+				ID:        3,
+				Name:      "method3",
+				Signature: "()",
+			},
+			{
+				ClassName: "class4",
+				ID:        4,
+				Name:      "method4",
+				Signature: "()",
+			},
+		},
+		StartTime: 398635355383000,
+		Threads: []AndroidThread{
+			{
+				ID:   1,
+				Name: "main",
+			},
+		},
+	}
+	missingExitEventsTrace = Android{
+		Clock: "Dual",
+		Events: []AndroidEvent{
+			{
+				Action:   "Enter",
+				ThreadID: 1,
+				MethodID: 1,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 1000,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Enter",
+				ThreadID: 1,
+				MethodID: 2,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 1000,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Exit",
+				ThreadID: 1,
+				MethodID: 1,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 2000,
+						},
+					},
+				},
+			},
+			{
+				Action:   "Enter",
+				ThreadID: 1,
+				MethodID: 1,
+				Time: EventTime{
+					Monotonic: EventMonotonic{
+						Wall: Duration{
+							Nanos: 3000,
+						},
+					},
+				},
+			},
+		},
+		Methods: []AndroidMethod{
+			{
+				ClassName: "class1",
+				ID:        1,
+				Name:      "method1",
+				Signature: "()",
+			},
+			{
+				ClassName: "class2",
+				ID:        2,
+				Name:      "method2",
+				Signature: "()",
+			},
+		},
+		StartTime: 398635355383000,
+		Threads: []AndroidThread{
+			{
+				ID:   1,
+				Name: "main",
+			},
+		},
+	}
+)
+
+func TestSpeedscope(t *testing.T) {
+	tests := []struct {
+		name  string
+		trace Android
+		want  speedscope.Output
+	}{
+		{
+			name:  "Build speedscope with missing exit events",
+			trace: missingExitEventsTrace,
+			want: speedscope.Output{
+				AndroidClock: "Dual",
+				Profiles: []any{
+					&speedscope.EventedProfile{
+						EndValue: 3000,
+						Events: []speedscope.Event{
+							{Type: "O", Frame: 0, At: 1000},
+							{Type: "O", Frame: 1, At: 1000},
+							{Type: "C", Frame: 1, At: 2000},
+							{Type: "C", Frame: 0, At: 2000},
+							{Type: "O", Frame: 0, At: 3000},
+							{Type: "C", Frame: 0, At: 3000},
+						},
+						Name:       "main",
+						StartValue: 1000,
+						ThreadID:   1,
+						Type:       "evented",
+						Unit:       "nanoseconds",
+					},
+				},
+				Shared: speedscope.SharedData{
+					Frames: []speedscope.Frame{
+						{Image: "class1", IsApplication: true, Name: "class1.method1()"},
+						{Image: "class2", IsApplication: true, Name: "class2.method2()"},
+					},
+				},
+			},
+		},
+		{
+			name:  "Build speedscope with missing enter events",
+			trace: missingEnterEventsTrace,
+			want: speedscope.Output{
+				AndroidClock: "Dual",
+				Profiles: []any{
+					&speedscope.EventedProfile{
+						EndValue: 3000,
+						Events: []speedscope.Event{
+							{Type: "O", Frame: 0, At: 1000},
+							{Type: "O", Frame: 2, At: 1500},
+							{Type: "O", Frame: 3, At: 1750},
+							{Type: "C", Frame: 3, At: 2250},
+							{Type: "C", Frame: 2, At: 2500},
+							{Type: "C", Frame: 0, At: 3000},
+						},
+						Name:       "main",
+						StartValue: 1000,
+						ThreadID:   1,
+						Type:       "evented",
+						Unit:       "nanoseconds",
+					},
+				},
+				Shared: speedscope.SharedData{
+					Frames: []speedscope.Frame{
+						{Image: "class1", Name: "class1.method1()", IsApplication: true},
+						{Image: "class2", Name: "class2.method2()", IsApplication: true},
+						{Image: "class3", Name: "class3.method3()", IsApplication: true},
+						{Image: "class4", Name: "class4.method4()", IsApplication: true},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output, err := test.trace.Speedscope()
+			if err != nil {
+				t.Fatalf("couldn't generate speedscope format: %+v", err)
+			}
+			if diff := testutil.Diff(output, test.want); diff != "" {
+				t.Fatalf("Result mismatch: got - want +\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestCallTrees(t *testing.T) {
+	tests := []struct {
+		name  string
+		trace Android
+		want  map[uint64][]*nodetree.Node
+	}{
+		{
+			name:  "Build call trees with missing exit events",
+			trace: missingExitEventsTrace,
+			want: map[uint64][]*nodetree.Node{
+				1: {
+					{
+						DurationNS:    1000,
+						IsApplication: true,
+						EndNS:         2000,
+						StartNS:       1000,
+						Name:          "class1.method1()",
+						Package:       "class1",
+						SampleCount:   1,
+						Frame: frame.Frame{
+							Function: "class1.method1()",
+							InApp:    &testutil.True,
+							MethodID: 1,
+							Package:  "class1",
+						},
+						Children: []*nodetree.Node{
+							{
+								DurationNS:    1000,
+								IsApplication: true,
+								Name:          "class2.method2()",
+								Package:       "class2",
+								EndNS:         2000,
+								StartNS:       1000,
+								SampleCount:   1,
+								Frame: frame.Frame{
+									Function: "class2.method2()",
+									InApp:    &testutil.True,
+									MethodID: 2,
+									Package:  "class2",
+								},
+							},
+						},
+					},
+					{
+						DurationNS:    0,
+						IsApplication: true,
+						Name:          "class1.method1()",
+						Package:       "class1",
+						EndNS:         3000,
+						StartNS:       3000,
+						Frame: frame.Frame{
+							Function: "class1.method1()",
+							MethodID: 1,
+							InApp:    &testutil.True,
+							Package:  "class1",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "Build call trees with missing enter events",
+			trace: missingEnterEventsTrace,
+			want: map[uint64][]*nodetree.Node{
+				1: {
+					{
+						DurationNS:    2000,
+						IsApplication: true,
+						EndNS:         3000,
+						SampleCount:   1,
+						StartNS:       1000,
+						Package:       "class1",
+						Name:          "class1.method1()",
+						Frame: frame.Frame{
+							Function: "class1.method1()",
+							InApp:    &testutil.True,
+							MethodID: 1,
+							Package:  "class1",
+						},
+						Children: []*nodetree.Node{
+							{
+								DurationNS:    1000,
+								IsApplication: true,
+								EndNS:         2500,
+								SampleCount:   1,
+								StartNS:       1500,
+								Package:       "class3",
+								Name:          "class3.method3()",
+								Frame: frame.Frame{
+									Function: "class3.method3()",
+									InApp:    &testutil.True,
+									MethodID: 3,
+									Package:  "class3",
+								},
+								Children: []*nodetree.Node{
+									{
+										DurationNS:    500,
+										IsApplication: true,
+										EndNS:         2250,
+										SampleCount:   1,
+										StartNS:       1750,
+										Package:       "class4",
+										Name:          "class4.method4()",
+										Frame: frame.Frame{
+											Function: "class4.method4()",
+											InApp:    &testutil.True,
+											MethodID: 4,
+											Package:  "class4",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	options := cmp.Options{
+		cmpopts.IgnoreFields(nodetree.Node{}, "Fingerprint", "ProfileIDs"),
+		cmpopts.IgnoreFields(frame.Frame{}, "File"),
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if diff := testutil.Diff(test.trace.CallTrees(), test.want, options); diff != "" {
+				t.Fatalf("Result mismatch: got - want +\n%s", diff)
+			}
+		})
+	}
+}
