85cdb2ef4122609311605480d3fbf7475e15e46a
Pierre Massat <pierre.massat@sentry.io>
2023-09-26 12:04:26 -0400

    Do not look for cronjobs

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -4,7 +4,6 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 
 /devinfra/scripts/k8s/k8stunnel
 /devinfra/scripts/k8s/k8s-deploy.py \
-	--type="cronjob" \
 	--label-selector="${LABEL_SELECTOR}" \
 	--image="us.gcr.io/internal-sentry/vroom:${GO_REVISION_VROOM_REPO}" \
 	--container-name="vroom"
