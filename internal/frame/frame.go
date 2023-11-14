dee04e693c3656e64671370f223f182e133b12e3
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2023-11-14 16:11:37 +0100

    Fix typo

--- internal/frame/frame.go
+++ internal/frame/frame.go
@@ -39,7 +39,7 @@ type (
 		Status          string `json:"status,omitempty"`
 		SymAddr         string `json:"sym_addr,omitempty"`
 		Symbol          string `json:"symbol,omitempty"`
-		Patform         string `json:"platform,omitempty"`
+		Platform        string `json:"platform,omitempty"`
 	}
 
 	Data struct {
