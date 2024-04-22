a3cf39d4b3721d17e7d3b24abdc83a17301f1ba9
Pierre Massat <pierre.massat@sentry.io>
2024-04-22 10:24:19 -0400

    Fix image names

--- gocd/templates/bash/check-cloudbuild.sh
+++ gocd/templates/bash/check-cloudbuild.sh
@@ -3,4 +3,4 @@
 /devinfra/scripts/checks/googlecloud/checkcloudbuild.py \
 	${GO_REVISION_VROOM_REPO} \
 	"sentryio" \
-	"us-central1-docker.pkg.dev/sentryio/vroom"
+	"us-central1-docker.pkg.dev/sentryio/vroom/vroom"
--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -5,5 +5,5 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 /devinfra/scripts/k8s/k8stunnel
 /devinfra/scripts/k8s/k8s-deploy.py \
 	--label-selector="${LABEL_SELECTOR}" \
-	--image="us-central1-docker.pkg.dev/sentryio/vroom:${GO_REVISION_VROOM_REPO}" \
+	--image="us-central1-docker.pkg.dev/sentryio/vroom/vroom:${GO_REVISION_VROOM_REPO}" \
 	--container-name="vroom"
