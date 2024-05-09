e4f7c5f4f51b51924a4606acbbad153e4d55d891
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-05-09 10:16:45 -0400

    set logger for sentry client and replace zerolog with slog

--- cmd/vroom/main.go
+++ cmd/vroom/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -16,7 +18,6 @@ import (
 	sentryhttp "github.com/getsentry/sentry-go/http"
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/julienschmidt/httprouter"
-	"github.com/rs/zerolog/log"
 	"github.com/segmentio/kafka-go"
 	"gocloud.dev/blob"
 	_ "gocloud.dev/blob/azureblob"
@@ -44,7 +45,9 @@ type environment struct {
 	metricsClient *http.Client
 }
 
-var release string
+var (
+	release string
+)
 
 const (
 	KiB int64 = 1024
@@ -190,7 +193,7 @@ func main() {
 
 	env, err := newEnvironment()
 	if err != nil {
-		log.Fatal().Err(err).Msg("error setting up environment")
+		log.Fatal("error setting up environment", err)
 	}
 
 	err = sentry.Init(sentry.ClientOptions{
@@ -215,13 +218,14 @@ func main() {
 		},
 	})
 	if err != nil {
-		log.Fatal().Err(err).Msg("can't initialize sentry")
+		log.Fatal("can't initialize sentry", err)
 	}
+	sentry.Logger = slog.NewLogLogger(slog.NewJSONHandler(os.Stderr, nil), slog.LevelError)
 
 	router, err := env.newRouter()
 	if err != nil {
 		sentry.CaptureException(err)
-		log.Fatal().Err(err).Msg("error setting up the router")
+		log.Fatal("error setting up the router", err)
 	}
 
 	server := http.Server{
@@ -241,7 +245,7 @@ func main() {
 
 		if err := server.Shutdown(cctx); err != nil {
 			sentry.CaptureException(err)
-			log.Err(err).Msg("error shutting down server")
+			slog.Error("error shutting down server", "err", err)
 		}
 
 		close(waitForShutdown)
@@ -250,7 +254,7 @@ func main() {
 	err = server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
 		sentry.CaptureException(err)
-		log.Err(err).Msg("server failed")
+		slog.Error("server failed", "err", err)
 	}
 
 	<-waitForShutdown
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/getsentry/vroom
 go 1.21
 
 require (
-	cloud.google.com/go/compute/metadata v0.2.3
 	cloud.google.com/go/storage v1.29.0
 	github.com/CAFxX/httpcompression v0.0.8
 	github.com/andybalholm/brotli v1.1.0
@@ -28,6 +27,7 @@ require (
 require (
 	cloud.google.com/go v0.110.0 // indirect
 	cloud.google.com/go/compute v1.19.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v0.13.0 // indirect
 	cloud.google.com/go/pubsub v1.30.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.3.1 // indirect
