d7491afdd309dcb2ce5131d41a29ea28fdae216e
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-05-09 15:22:11 -0400

    remove groups name var

--- internal/logutil/logutil.go
+++ internal/logutil/logutil.go
@@ -8,7 +8,7 @@ import (
 func ConfigureLogger() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
 			if a.Key == slog.MessageKey {
 				a.Key = "message"
 			}
