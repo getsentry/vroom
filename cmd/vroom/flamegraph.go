6bd984ab9c0c98778a0989f7dfd7632f6084f7a3
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-06-12 16:34:22 +0200

    ref: remove unused flamegraph endpoint (dead code) (#472)
    * remove unused flamegraph endpoint (dead code)
    
    This was an older implementation where the profile IDs where fetched
    directly in vroom.
    This was dismissed in favour of the next version that fetched the
    profile IDs in sentry and posted them to vroom, whose only task it to
    download and aggregate the data.
    
    * update changelog

--- cmd/vroom/flamegraph.go
+++ cmd/vroom/flamegraph.go
@@ -11,83 +11,12 @@ import (
 	"github.com/julienschmidt/httprouter"
 
 	"github.com/getsentry/vroom/internal/flamegraph"
-	"github.com/getsentry/vroom/internal/snubautil"
 )
 
 const (
 	minNumWorkers int           = 5
 	timeout       time.Duration = time.Second * 5
 )
-
-func (env *environment) getFlamegraph(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	hub := sentry.GetHubFromContext(ctx)
-	ps := httprouter.ParamsFromContext(ctx)
-	rawOrganizationID := ps.ByName("organization_id")
-	organizationID, err := strconv.ParseUint(rawOrganizationID, 10, 64)
-	if err != nil {
-		hub.CaptureException(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	hub.Scope().SetTag("organization_id", rawOrganizationID)
-
-	rawProjectID := ps.ByName("project_id")
-	projectID, err := strconv.ParseUint(rawProjectID, 10, 64)
-	if err != nil {
-		sentry.CaptureException(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	urlValues := r.URL.Query()
-	urlValues.Set("project_id", rawProjectID)
-	r.URL.RawQuery = urlValues.Encode()
-
-	hub.Scope().SetTag("project_id", rawProjectID)
-
-	sqb, err := env.profilesQueryBuilderFromRequest(ctx, r.URL.Query(), organizationID)
-	if err != nil {
-		hub.CaptureException(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	s := sentry.StartSpan(ctx, "snuba.read")
-	profileIDs, err := snubautil.GetProfileIDs(organizationID, 100, sqb)
-	if err != nil {
-		s.Finish()
-		hub.CaptureException(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	hub.Scope().SetTag("fetched_profiles", strconv.Itoa(len(profileIDs)))
-	s.Finish()
-
-	s = sentry.StartSpan(ctx, "processing")
-	speedscope, err := flamegraph.GetFlamegraphFromProfiles(ctx, env.storage, organizationID, projectID, profileIDs, nil, minNumWorkers, timeout)
-	if err != nil {
-		s.Finish()
-		hub.CaptureException(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	s.Finish()
-
-	s = sentry.StartSpan(ctx, "json.marshal")
-	defer s.Finish()
-	b, err := json.Marshal(speedscope)
-	if err != nil {
-		hub.CaptureException(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(b)
-}
 
 type postFlamegraphFromProfileIDs struct {
 	ProfileIDs []string `json:"profile_ids"`
