fceaaf3a272229083337f28499ccda4df1c8abcb
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-07-26 15:44:08 +0200

    add GetMetricsFromCandidates

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,11 +1,18 @@
 package metrics
 
 import (
+	"context"
 	"errors"
 	"math"
 	"sort"
 
+	"github.com/getsentry/sentry-go"
+	"github.com/getsentry/vroom/internal/chunk"
+	"github.com/getsentry/vroom/internal/flamegraph"
 	"github.com/getsentry/vroom/internal/nodetree"
+	"github.com/getsentry/vroom/internal/profile"
+	"github.com/getsentry/vroom/internal/storageutil"
+	"gocloud.dev/blob"
 )
 
 type FunctionsMetadata struct {
@@ -115,4 +122,158 @@ func quantile(values []uint64, q float64) (uint64, error) {
 	}
 	index := int(math.Ceil(float64(len(values))*q)) - 1
 	return values[index], nil
+}
+
+func ExtractFunctionsFromCallTrees(
+	callTrees map[uint64][]*nodetree.Node,
+) []nodetree.CallTreeFunction {
+	functions := make(map[uint32]nodetree.CallTreeFunction, 0)
+
+	for _, callTreesForThread := range callTrees {
+		for _, callTree := range callTreesForThread {
+			callTree.CollectFunctions(functions)
+		}
+	}
+
+	functionsList := make([]nodetree.CallTreeFunction, 0, len(functions))
+	for _, function := range functions {
+		if function.SampleCount <= 1 {
+			// if there's only ever a single sample for this function in
+			// the profile, we skip over it to reduce the amount of data
+			continue
+		}
+		functionsList = append(functionsList, function)
+	}
+
+	// sort the list in descending order, and take the top N results
+	sort.SliceStable(functionsList, func(i, j int) bool {
+		return functionsList[i].SumSelfTimeNS > functionsList[j].SumSelfTimeNS
+	})
+
+	return functionsList
+}
+
+func CapAndFilterFunctions(functions []nodetree.CallTreeFunction, maxUniqueFunctionsPerProfile int, filterSystemFrames bool) []nodetree.CallTreeFunction {
+	if !filterSystemFrames {
+		if len(functions) > maxUniqueFunctionsPerProfile {
+			return functions[:maxUniqueFunctionsPerProfile]
+		}
+		return functions
+	}
+	appFunctions := make([]nodetree.CallTreeFunction, 0, min(maxUniqueFunctionsPerProfile, len(functions)))
+	for _, f := range functions {
+		if !f.InApp {
+			continue
+		}
+		appFunctions = append(appFunctions, f)
+		if len(appFunctions) == maxUniqueFunctionsPerProfile {
+			break
+		}
+	}
+	return appFunctions
+}
+
+func GetMetricsFromCandidates(
+	ctx context.Context,
+	storage *blob.Bucket,
+	organizationID uint64,
+	transactionProfileCandidates []flamegraph.TransactionProfileCandidate,
+	continuousProfileCandidates []flamegraph.ContinuousProfileCandidate,
+	jobs chan storageutil.ReadJob,
+	maxUniqueFunctions uint,
+	maxNumOfExamples uint,
+) ([]FunctionMetrics, error) {
+	hub := sentry.GetHubFromContext(ctx)
+
+	numCandidates := len(transactionProfileCandidates) + len(continuousProfileCandidates)
+
+	results := make(chan storageutil.ReadJobResult, numCandidates)
+	defer close(results)
+
+	for _, candidate := range transactionProfileCandidates {
+		jobs <- profile.ReadJob{
+			Ctx:            ctx,
+			OrganizationID: organizationID,
+			ProjectID:      candidate.ProjectID,
+			ProfileID:      candidate.ProfileID,
+			Storage:        storage,
+			Result:         results,
+		}
+	}
+
+	for _, candidate := range continuousProfileCandidates {
+		jobs <- chunk.ReadJob{
+			Ctx:            ctx,
+			OrganizationID: organizationID,
+			ProjectID:      candidate.ProjectID,
+			ProfilerID:     candidate.ProfilerID,
+			ChunkID:        candidate.ChunkID,
+			ThreadID:       candidate.ThreadID,
+			Start:          candidate.Start,
+			End:            candidate.End,
+			Storage:        storage,
+			Result:         results,
+		}
+	}
+
+	ma := NewAggregator(maxUniqueFunctions, maxNumOfExamples)
+
+	for i := 0; i < numCandidates; i++ {
+		res := <-results
+
+		err := res.Error()
+		if err != nil {
+			if errors.Is(err, storageutil.ErrObjectNotFound) {
+				continue
+			}
+			if errors.Is(err, context.DeadlineExceeded) {
+				return nil, err
+			}
+			if hub != nil {
+				hub.CaptureException(err)
+			}
+			continue
+		}
+
+		if result, ok := res.(profile.ReadJobResult); ok {
+			profileCallTrees, err := result.Profile.CallTrees()
+			if err != nil {
+				hub.CaptureException(err)
+				continue
+			}
+
+			functions := CapAndFilterFunctions(ExtractFunctionsFromCallTrees(profileCallTrees), int(ma.MaxUniqueFunctions), true)
+			ma.AddFunctions(functions, result.Profile.ID())
+		} else if result, ok := res.(chunk.ReadJobResult); ok {
+			chunkCallTrees, err := result.Chunk.CallTrees(result.ThreadID)
+			if err != nil {
+				hub.CaptureException(err)
+				continue
+			}
+			intChunkCallTrees := make(map[uint64][]*nodetree.Node)
+			i := uint64(0)
+			for _, v := range chunkCallTrees {
+				// reald TID here doesn't really matter as it's then
+				// discarded (not used) by ExtractFunctionsFromCallTrees.
+				// Here we're only assigning a random uint to make it compatible
+				// with ExtractFunctionsFromCallTrees which expects an
+				// uint64 -> []*nodetree.Node based on sample V1 int tid
+				// the TID.
+				//
+				// We could even refactor ExtractFunctionsFromCallTrees
+				// to simply accept []*nodetree.Node instead of a map
+				// but we'd end up moving the iteration from map to a slice
+				// somewhere else in the code.
+				intChunkCallTrees[i] = v
+				i++
+			}
+			functions := CapAndFilterFunctions(ExtractFunctionsFromCallTrees(intChunkCallTrees), int(ma.MaxUniqueFunctions), true)
+			ma.AddFunctions(functions, result.Chunk.ID)
+		} else {
+			// this should never happen
+			return nil, errors.New("unexpected result from storage")
+		}
+	}
+
+	return ma.ToMetrics(), nil
 }
