e85db12a39eeb7efce885eda73e205947a3e46b1
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-10-08 11:34:41 +0200

    stop writing metrics into generic metrics for transaction-based profiling

--- cmd/vroom/utils.go
+++ cmd/vroom/utils.go
@@ -13,7 +13,6 @@ import (
 	"github.com/getsentry/sentry-go"
 	"github.com/getsentry/vroom/internal/chunk"
 	"github.com/getsentry/vroom/internal/nodetree"
-	"github.com/getsentry/vroom/internal/profile"
 )
 
 func getFlamegraphNumWorkers(numProfiles, minNumWorkers int) int {
@@ -22,48 +21,6 @@ func getFlamegraphNumWorkers(numProfiles, minNumWorkers int) int {
 	}
 	v := int(math.Ceil((float64(numProfiles) / 100) * float64(minNumWorkers)))
 	return max(v, minNumWorkers)
-}
-
-func extractMetricsFromFunctions(p *profile.Profile, functions []nodetree.CallTreeFunction) ([]sentry.Metric, []MetricSummary) {
-	metrics := make([]sentry.Metric, 0, len(functions))
-	metricsSummary := make([]MetricSummary, 0, len(functions))
-
-	for _, function := range functions {
-		if len(function.SelfTimesNS) == 0 {
-			continue
-		}
-		tags := map[string]string{
-			"project_id":     strconv.FormatUint(p.ProjectID(), 10),
-			"fingerprint":    strconv.FormatUint(uint64(function.Fingerprint), 10),
-			"function":       function.Function,
-			"package":        function.Package,
-			"is_application": strconv.FormatBool(function.InApp),
-			"platform":       string(p.Platform()),
-			"environment":    p.Environment(),
-			"release":        p.Release(),
-			"profile_type":   "transaction",
-		}
-		duration := float64(function.SelfTimesNS[0] / 1e6)
-		summary := MetricSummary{
-			Min:   duration,
-			Max:   duration,
-			Sum:   duration,
-			Count: 1,
-		}
-		dm := sentry.NewDistributionMetric("profiles/function.duration", sentry.MilliSecond(), tags, p.Metadata().Timestamp, duration)
-		// loop remaining selfTime durations
-		for i := 1; i < len(function.SelfTimesNS); i++ {
-			duration := float64(function.SelfTimesNS[i] / 1e6)
-			dm.Add(duration)
-			summary.Min = min(summary.Min, duration)
-			summary.Max = max(summary.Max, duration)
-			summary.Sum = summary.Sum + duration
-			summary.Count = summary.Count + 1
-		}
-		metrics = append(metrics, dm)
-		metricsSummary = append(metricsSummary, summary)
-	}
-	return metrics, metricsSummary
 }
 
 func sendMetrics(ctx context.Context, dsn string, metrics []sentry.Metric, mClient *http.Client) {
