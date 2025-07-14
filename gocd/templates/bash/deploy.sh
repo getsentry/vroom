638060d8bf070f89000eba478790fa0d701f2575
mchen-sentry <icswon1108@gmail.com>
2025-07-14 10:23:58 -0700

    filename

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -2,7 +2,7 @@
 
 eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 
-/devinfra/scripts/k8s/k8stunnel
+/devinfra/scripts/get-cluster-credentials
 k8s-deploy \
 	--label-selector="${LABEL_SELECTOR}" \
 	--image="us-central1-docker.pkg.dev/sentryio/vroom/vroom:${GO_REVISION_VROOM_REPO}" \
